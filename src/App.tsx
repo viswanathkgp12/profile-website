e3341e3e5107da60ce2d263caa06aab926fdc2f5
Viswanath Kapavarapu <viswanath.iit@gmail.com>
2025-08-24 23:59:32 +0530

    fix lint err

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,6 @@ import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Footer from "./components/layout/footer";
 import Header from "./components/layout/header";
 import { Home } from "./components/Home";
-import { Posts } from "./components/Posts";
 import React from "react";
 
 function App() {
@@ -16,7 +15,6 @@ function App() {
           <Header />
           <Switch>
             <Route exact path="/" component={Home}></Route>
-            {/* <Route path="/posts" component={Posts}></Route> */}
           </Switch>
           <Footer />
         </div>
